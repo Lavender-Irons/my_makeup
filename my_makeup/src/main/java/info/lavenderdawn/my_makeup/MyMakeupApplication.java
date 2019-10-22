728bf035d5b31cb592f121e1a97d913faa3e4a25
Lavender <lavvie@users.noreply.github.com>
2019-10-22 18:10:59 -0500

    initial commit

---
+++ my_makeup/src/main/java/info/lavenderdawn/my_makeup/MyMakeupApplication.java
@@ -0,0 +1,13 @@
+package info.lavenderdawn.my_makeup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMakeupApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMakeupApplication.class, args);
+    }
+
+}
---
+++ my_makeup/src/test/java/info/lavenderdawn/my_makeup/MyMakeupApplicationTests.java
@@ -0,0 +1,13 @@
+package info.lavenderdawn.my_makeup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMakeupApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
